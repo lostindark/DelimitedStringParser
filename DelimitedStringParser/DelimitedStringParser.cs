0492a96256b05c9f96cb05b6f29c422249eca3c4
Teddy Zhang <lostindark@live.com>
2016-08-18 22:51:48 -0700

    Create project

---
+++ DelimitedStringParser/DelimitedStringParser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DelimitedStringParser
+{
+    public class DelimitedStringParser<T>
+    {
+        private static readonly string delimiter;
+        private static readonly bool isVersionedString = false;
+
+        /// <summary>
+        /// Static constructor.
+        /// It check the schema of the object and generate field parser.
+        /// </summary>
+        static DelimitedStringParser()
+        {
+            
+        }
+    }
+}
---
+++ DelimitedStringParserTests/DelimitedStringParserTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DelimitedStringParser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DelimitedStringParser.Tests
+{
+    [TestClass()]
+    public class DelimitedStringParserTests
+    {
+    }
+}
